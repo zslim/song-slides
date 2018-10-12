54c010b79c28911fe4b65de60036969631987910
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-10-12 16:40:15 +0200

    created table with songs on index'

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import data_manager
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    songs_to_list = data_manager.get_songs_with_slides()
+    return render_template("index.html", list_of_songs=songs_to_list)
 
 
 if __name__ == '__main__':
--- data_manager.py
+++ data_manager.py
@@ -25,7 +25,7 @@ def save_new_slide(cursor, dict_of_slide):
 @db_connection.connection_handler
 def get_songs_with_slides(cursor):
     cursor.execute("""
-                    SELECT songs.id, title, lyrics
+                    SELECT DISTINCT songs.id, title, lyrics
                     FROM songs LEFT JOIN slides s2 on songs.id = s2.song_id
                     WHERE s2.id IS NOT NULL
                     ORDER BY songs.id;
