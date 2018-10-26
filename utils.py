85ab3b8cf1b8864a890020a6dcc20041f35b50ea
Zsofia Limbek <zsofia.limbek@gmail.com>
2018-10-26 14:13:13 +0200

    added list flattening function

--- utils.py
+++ utils.py
@@ -5,3 +5,14 @@ def strip_newlines_from_edges(string):
     stripped_from_start = re.sub("^\n", "", string)
     stripped_from_both = re.sub("\n$", "", stripped_from_start)
     return stripped_from_both
+
+
+def flatten_list(nested_list):
+    flat_list = []
+    for element in nested_list:
+        if isinstance(element, list):
+            for item in element:
+                flat_list.append(item)
+        else:
+            flat_list.append(element)
+    return flat_list
